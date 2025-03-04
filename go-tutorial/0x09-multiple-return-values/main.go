02107611e93dafcfeb87712ff21765c28679f9d7
DanielJohn17 <danielyohannes38@gmail.com>
2025-03-04 20:10:16 +0300

    multiple return values

---
+++ go-tutorial/0x09-multiple-return-values/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func getInitials(n string) (string, string) {
+	s := strings.ToUpper(n)
+	names := strings.Split(s, " ")
+
+	var initials []string
+	for _, value := range names {
+		initials = append(initials, value[:1])
+	}
+
+	if len(initials) > 1 {
+		return initials[0], initials[1]
+	}
+
+	return initials[0], "_"
+}
+
+func main() {
+
+	fmt.Println(getInitials("Daniel John"))
+
+  fmt.Println(getInitials("Daniel"))
+}
