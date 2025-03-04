935ef94575197364adda0b90d6dc65d0faa30a94
DanielJohn17 <danielyohannes38@gmail.com>
2025-03-04 23:23:47 +0300

    used receiver functions on mybill struct

--- go-tutorial/bills-project/main.go
+++ go-tutorial/bills-project/main.go
@@ -3,7 +3,14 @@ package main
 import "fmt"
 
 func main() {
-  mybill := newBill("mario's bill")
+	mybill := newBill("mario's bill")
 
-  fmt.Println(mybill.format())
+	mybill.addItem("onion soup", 4.50)
+	mybill.addItem("veg pie", 8.95)
+	mybill.addItem("toffee pudding", 4.95)
+	mybill.addItem("coffee", 3.25)
+
+	mybill.updateTip(10)
+
+	fmt.Println(mybill.format())
 }
