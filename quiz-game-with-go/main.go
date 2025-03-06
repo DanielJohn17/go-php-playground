71cf2b0da69f61db2a29bf33e7b4f7db37a3411d
DanielJohn17 <danielyohannes38@gmail.com>
2025-03-06 23:04:11 +0300

    created getinput function to take input from the user

--- quiz-game-with-go/main.go
+++ quiz-game-with-go/main.go
@@ -1,23 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func getInput() (string, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, error := reader.ReadString('\n')
+
+	return strings.TrimSpace(input), error
+}
 
 func main() {
+
 	fmt.Println("Welcome to my quiz game!")
-	var name string
-	fmt.Scan(&name)
+	name, _ := getInput()
 
 	fmt.Printf("Hello, %v, welcome to the game!\n", name)
 
 	fmt.Printf("Enter your age: ")
-	var age uint
-	fmt.Scan(&age)
+	age_str, _ := getInput()
+	age, error := strconv.ParseInt(age_str, 10, 0)
 
+	if error != nil {
+		fmt.Println("Please enter a number")
+		return
+	}
 	if age >= 10 {
 		fmt.Println("Yay you can play!")
 	} else {
 		fmt.Println("Sorry, you are too young to play!")
-    return
+		return
+	}
+
+	fmt.Printf("What is better, the RTX 3080 or RTX 3090? ")
+	answer, _ := getInput()
+
+	if answer == "RTX 3090" {
+		fmt.Println("Correct!")
+	} else {
+		fmt.Println("Wrong!")
 	}
 
 }
