a87fbece6f76ea83de667a684863b98d36ebf592
DanielJohn17 <danielyohannes38@gmail.com>
2025-03-13 21:13:26 +0300

    loops

---
+++ php-tutorial/0x06-loops/index.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "For loop<br>";
+for ($i = 0; $i < 20; $i++) {
+    if ($i % 2 == 0) {
+        echo $i . "<br>";
+    }
+}
+
+echo "While loop<br>";
+while ($a <= 5) {
+    echo $a . "<br>";
+    $a++;
+}
