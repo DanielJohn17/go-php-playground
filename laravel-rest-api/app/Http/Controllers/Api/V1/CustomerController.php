47ec5963f0a39b606e3cdc9d6f31e8cfac8e403b
DanielJohn17 <danielyohannes38@gmail.com>
2025-03-31 20:05:44 +0300

    update customer

--- laravel-rest-api/app/Http/Controllers/Api/V1/CustomerController.php
+++ laravel-rest-api/app/Http/Controllers/Api/V1/CustomerController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 
 use App\Http\Requests\V1\StoreCustomerRequest;
-use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Requests\V1\UpdateCustomerRequest;
 use App\Models\Customer;
 
 use App\Http\Resources\V1\CustomerResource;
@@ -59,7 +59,7 @@ class CustomerController extends Controller
      */
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        //
+        $customer->update($request->all());
     }
 
     /**
