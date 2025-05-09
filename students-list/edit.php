107fd8dc47502be6bfa6df1bf8d6ee84edb92407
DanielJohn17 <danielyohannes38@gmail.com>
2025-05-09 23:23:40 +0300

    edit student data

---
+++ students-list/edit.php
@@ -0,0 +1,84 @@
+<?php
+
+require_once 'db.php';
+
+global $conn;
+
+// Ensure the connection is established
+if (!$conn->connect()) {
+    die("Database connection failed.");
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
+    $age = htmlspecialchars($_POST['age'], ENT_QUOTES, 'UTF-8');
+    $email = htmlspecialchars($_POST['email'], ENT_QUOTES, 'UTF-8');
+    $id = htmlspecialchars($_POST['id'], ENT_QUOTES, 'UTF-8');
+
+    if (empty($name) || empty($age) || empty($email)) {
+        echo "Please fill in all fields.";
+    } else {
+        if ($conn->update_data($id, $name, $email, $age)) {
+            header("Location: index.php");
+            exit();
+        } else {
+            echo "Failed to update student.";
+        }
+    }
+} else {
+    $id = $_GET['id'];
+    $student = $conn->fetch_student($id);
+    if (empty($student)) {
+        echo "Student not found.";
+        exit();
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>Student List</title>
+</head>
+
+<body class="w-screen h-screen bg-gray-100 flex flex-col">
+    <?php include './layout/nav.html'; ?>
+    <div class="flex-grow p-6 flex items-center justify-center">
+        <form action="edit.php" method="post" class="w-xl p-6 bg-white shadow-md rounded">
+            <h1 class="text-xl font-bold tracking-wide mb-5">Edit student</h1>
+            <input type="hidden" name="id" value="<?php echo $id; ?>" />
+            <div class="mb-4">
+                <label for="name" class="block text-gray-700">Name:</label>
+                <input type="text" id="name" name="name" class="border border-gray-300 p-2 w-full" placeholder="Enter name" value="<?php echo htmlspecialchars($student['name'], ENT_QUOTES, 'UTF-8'); ?>" required />
+            </div>
+
+            <div class="mb-4">
+                <label for="age" class="block text-gray-700">Age:</label>
+                <input type="number" id="age" name="age" class="border border-gray-300 p-2 w-full" placeholder="Enter age" value="<?php echo htmlspecialchars($student['age'], ENT_QUOTES, 'UTF-8'); ?>" required />
+            </div>
+
+            <div class="mb-4">
+                <label for="email" class="block text-gray-700">Email:</label>
+                <input type="email" id="email" name="email" class="border border-gray-300 p-2 w-full" placeholder="Enter email" value="<?php echo htmlspecialchars($student['email'], ENT_QUOTES, 'UTF-8'); ?>" required />
+            </div>
+
+            <div class="w-full flex justify-end items-center">
+                <button
+                    type="submit"
+                    name="submit"
+                    class="bg-gray-800 text-white px-4 py-2 rounded hover:bg-gray-600 transition duration-200 cursor-pointer">
+                    Update
+                </button>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
+<!-- <?php
+        $conn->close();
+        ?> -->
